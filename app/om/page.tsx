eeeb22d9575746af209b2c84198f2d1a38af1e4f
FyndBo <fyndbo.m@gmail.com>
2026-05-07 21:43:29 +0200

    Increase logo size on all devices

--- app/om/page.tsx
+++ app/om/page.tsx
@@ -11,12 +11,12 @@ export default function OmSida() {
       </div>
 
       <main className="relative z-10 max-w-4xl mx-auto px-4 sm:px-6 py-8 sm:py-16">
-        {/* Logga - större på mobil */}
+        {/* Logga - STÖRRE på alla enheter */}
         <div className="text-center mb-8 sm:mb-12">
           <img 
             src="/Fyndbo-blue-bkg.png" 
             alt="FyndBo.se" 
-            className="h-24 sm:h-28 md:h-32 lg:h-40 xl:h-48 2xl:h-56 mx-auto w-auto hover:scale-105 transition-transform duration-500" 
+            className="h-28 sm:h-32 md:h-40 lg:h-48 xl:h-56 2xl:h-64 mx-auto w-auto hover:scale-105 transition-transform duration-500 drop-shadow-xl" 
           />
         </div>
 
--- app/page.tsx
+++ app/page.tsx
@@ -73,12 +73,12 @@ export default function Home() {
       </div>
 
       <main className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 py-8 sm:py-12">
-        {/* Logga - centrerad, ingen ram */}
+        {/* Logga - STÖRRE på alla enheter */}
         <div className="flex justify-center pt-4 pb-6 sm:pt-8 sm:pb-16">
           <img 
             src="/Fyndbo-blue-bkg.png" 
             alt="FyndBo.se" 
-            className="h-20 sm:h-28 md:h-40 lg:h-48 xl:h-56 2xl:h-64 w-auto hover:scale-105 transition-transform duration-500" 
+            className="h-32 sm:h-40 md:h-48 lg:h-56 xl:h-64 2xl:h-72 w-auto hover:scale-105 transition-transform duration-500 drop-shadow-xl" 
           />
         </div>
 
